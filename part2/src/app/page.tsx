6c2382f7e2b940943d666329cc0e657c1875b879
Keerthy Baby <keerthybaby025@gmail.com>
2025-02-12 15:24:30 +0530

    React useState

--- part2/src/app/page.tsx
+++ part2/src/app/page.tsx
@@ -1,13 +1,15 @@
 // import PostList from "@/components/React_props/PostList";
 // import ChildrenPropexample from "@/components/React_Events/ChildrenPropexample/ChildrenPropexample";
-import EventExample from "@/components/React_Events/EventExample/EventExample";
+// import EventExample from "@/components/React_Events/EventExample/EventExample";
+import UseStateExample from "@/components/React_useState/UseStateExample"
 
 export default function Home() {
   return (
     <div>
       {/* <PostList /> */}
       {/* <ChildrenPropexample /> */}
-      <EventExample/>
+      {/* <EventExample/> */}
+      <UseStateExample/>
     </div>
   );
 }
