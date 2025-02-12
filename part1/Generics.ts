bc58682f9538851e2fc3d4cc89b9316fc2e666da
Keerthy Baby <keerthybaby025@gmail.com>
2025-02-12 11:24:12 +0530

    Generics

---
+++ part1/Generics.ts
@@ -0,0 +1,52 @@
+// GENERICS
+interface IAuthor {
+  id: number;
+  username: string;
+}
+
+interface ICategory {
+  id: number;
+  title: string;
+}
+
+interface IPost {
+  id: number;
+  title: string;
+  desc: string;
+  extra: IAuthor[] | ICategory[];
+}
+
+interface IPostBetter<T> {
+  id: number;
+  title: string;
+  desc: string;
+  extra: T[];
+}
+
+const testMe: IPostBetter<String> = {
+  id: 1,
+  title: "post title",
+  desc: "post desc",
+  extra: ["str", "str2"],
+};
+
+interface IPostEvenBetter<T extends object> {
+  id: number;
+  title: string;
+  desc: string;
+  extra: T[];
+}
+
+const testMe2: IPostEvenBetter<IAuthor> = {
+  id: 1,
+  title: "post title",
+  desc: "post desc",
+  extra: [{ id: 1, username: "john" }],
+};
+
+const testMe3: IPostEvenBetter<ICategory> = {
+  id: 1,
+  title: "post title",
+  desc: "post desc",
+  extra: [{ id: 1, title: "cat" }],
+};
